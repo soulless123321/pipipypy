2d5c2dc6ba87e67d01b68306642b4da550dc3fb1
soulless123321 <ilasemonaev32232@gmail.com>
2024-09-30 11:49:10 +0300

    Add files via upload

---
+++ практическая 5/Program.cs
@@ -0,0 +1,48 @@
+﻿//*************************************************************************
+//* практическая работа 5 
+//* Выполнил студент 2-ИСПд Шемонаев.И.А
+//* адание:написать программу определяющую счастливый билет
+//*************************************************************************
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace практическая_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Введите номер билета: ");
+            int ticket = Convert.ToInt32(Console.ReadLine());
+            if (ticket >= 100000 && ticket <= 999999)
+            {
+                int b = ticket / 100000;
+                int c = (ticket / 10000) % 10;
+                int d = (ticket / 1000) % 10;
+                int e = (ticket / 100) % 10;
+                int f = (ticket / 10) % 10;
+                int g = (ticket % 10);
+                if (b + c + d == e + f + g)
+
+                    Console.WriteLine("Билет счастливый");
+                else
+                    Console.WriteLine("не счастливый билет");
+            }
+            else
+                Console.WriteLine("Номер билета не шестизначный");
+                Console.ReadKey();
+
+            
+
+        }
+
+
+
+    }
+}
