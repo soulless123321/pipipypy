26977303b94460495c0c4e4135b8de0b0d8b1a44
soulless123321 <ilasemonaev32232@gmail.com>
2024-10-01 23:04:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿//*************************************************************************
+//* практическая работа 6 
+//* Выполнил:  студент 2-ИСПд Шемонаев.И.А
+//* Задание: переписать практическую 5 на switch
+//*************************************************************************
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace практическая_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер билета: ");
+            int ticket = Convert.ToInt32(Console.ReadLine());
+            switch (ticket >= 100000 && ticket <= 999999)
+            {
+                case true:
+                {
+                    int b = ticket / 100000;
+                    int c = (ticket / 10000) % 10;
+                    int d = (ticket / 1000) % 10;
+                    int e = (ticket / 100) % 10;
+                    int f = (ticket / 10) % 10;
+                    int g = (ticket % 10);
+                    switch (b + c + d == e + f + g)
+                    {
+                        case true:
+                        {
+                            Console.WriteLine("Билет счастливый");
+                            break;
+                        }
+                        case false:
+                        {
+                            Console.WriteLine("не счастливый билет");
+                            break;
+                        }
+                    }
+                        break;
+                    }
+                    case false:
+                    {
+                        Console.WriteLine("Номер билета не шестизначный");
+                        break;
+                    }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Практическая номер 4/Практическая номер 4/Program.cs
@@ -0,0 +1,34 @@
+﻿//******************************************************
+//*Практическая № 4
+//*Выполнил Шемонаев.И.А 2-ИСПд
+//*Задание: Написать код по функции
+//******************************************************
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая_номер_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double e = 2.718;                         //Число Эйлера
+            Console.Write("Введите значение x: ");
+            double x = Double.Parse(Console.ReadLine());    //Ввел число X и перевел в double  
+            Console.Write("Введите значение b: ");
+            double b = Double.Parse(Console.ReadLine()); 
+            double v1 = Math.Cos(Math.Pow(x,3));
+            double v2 = Math.Pow(b + Math.Atan(x * 2),0.2);
+            double v3 = 3 * Math.Pow(e, Math.Sqrt(b * x));
+            double v4 = v2 / v3;
+            double v5 = Math.Abs(v4);
+            double v6 = v1 + v5;                            //Ответ
+            Console.WriteLine($"Ответ: {Math.Round(v6, 5)}");
+            Console.ReadKey();
+        }
+    }
+}
