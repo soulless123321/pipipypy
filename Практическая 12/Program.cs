939bd1e021c2c9ee20dd07cfa0f03e1d8b5615b5
soulless123321 <ilasemonaev32232@gmail.com>
2024-10-24 16:53:29 +0300

    Add files via upload

---
+++ Практическая 12/Program.cs
@@ -0,0 +1,71 @@
+﻿//***********************************************************************
+//*Практическая № 4
+//*Выполнил: Шемонаев.И.А 2-ИСПд
+//*Задание: Написвть код по функции
+//***********************************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая_12
+{
+    internal class Program
+    {
+        static void ExeptionWrait(string extext)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Ошибка: " + extext);
+            Console.ResetColor();
+        }
+
+        static string ToUpperLatin(string str)
+        {
+            char[] result = new char[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                char c = str[i];
+                if (c >= 'a' && c <= 'z') //Проверка на латинские буквы в нижнем регистре
+                {
+                    result[i] = (char)(c - 32); //Преобразование в верхний регистр
+                }
+                else
+                {
+                    result[i] = c; //Оставляем символ без изминений
+                }
+            }
+            return new string(result);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте");
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите строку (или 'стоп' для завершения): ");
+                    string input = Console.ReadLine();
+                    if (input.ToLower() == "стоп") break; //Прекращение работы кода
+                    if (string.IsNullOrWhiteSpace(input))
+                    {
+                        
+                        throw new ArgumentNullException("Строка не должна быть пустой или состоять только из пробелов.");
+                        
+                    }
+                    string result = ToUpperLatin(input); // Вызов функции преобразования
+                    Console.WriteLine("Результат: " + result); // Вывод результата               
+                }
+                catch (FormatException ex)
+                {
+                    ExeptionWrait(ex.Message);
+                }
+                
+                catch (Exception ex)
+                {
+                    ExeptionWrait(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Практическая 12/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Практическая 12")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Практическая 12")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("671787dd-afc1-4c2d-b415-33c407082f77")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
