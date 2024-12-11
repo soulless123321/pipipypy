f743bab01328b1ad5d58498b84a765a26021da16
soulless123321 <ilasemonaev32232@gmail.com>
2024-12-11 03:13:22 +0300

    Add files via upload

---
+++ Практическая работа №17/Практическая работа №17/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая_работа__17
+{
+    class WaterTransport
+    {
+        string Name { get; set; } // Название водного средства
+        int Capacity { get; set; } // Вместимость в людях
+        double Speed { get; set; } // Скорость в км/ч
+
+        // Метод для ввода данных о водном средстве
+        void InputData()
+        {
+            Console.Write("Введите название водного средства: ");
+            Name = Console.ReadLine();
+
+            // Проверка ввода вместимости
+            while (true)
+            {
+                Console.Write("Введите вместимость: ");
+                if (int.TryParse(Console.ReadLine(), out int capacity))
+                {
+                    Capacity = capacity;
+                    break; // Выход из цикла при корректном вводе
+                }
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ошибка: введите корректное число для вместимости.");
+                Console.ResetColor();
+            }
+
+            // Проверка ввода скорости
+            while (true)
+            {
+                Console.Write("Введите скорость: ");
+                if (double.TryParse(Console.ReadLine(), out double speed) && speed > 0)
+                {
+                    Speed = speed;
+                    break; // Выход из цикла при корректном вводе
+                }
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ошибка: введите корректное положительное число для скорости.");
+                Console.ResetColor();
+            }
+        }
+
+        // Метод для отображения данных о водном средстве
+        void DisplayData()
+        {
+            Console.WriteLine($"Название: {Name}");
+            Console.WriteLine($"Вместимость: {Capacity} человек");
+            Console.WriteLine($"Скорость: {Speed} км/ч");
+        }
+
+        // Метод для расчета времени в пути
+        void CalculateTravelTime(double distance)
+        {
+            double time = distance / Speed; // Расчет времени
+            Console.WriteLine($"Время в пути на {distance} км: {time:F2} часов");
+        }
+
+        // Метод для выполнения всех операций
+        public void Execute()
+        {
+            InputData(); // Запрашиваем данные у пользователя
+            DisplayData(); // Отображаем введенные данные
+
+            // Запрашиваем расстояние для расчета времени в пути
+            Console.Write("Введите расстояние для расчета времени в пути: ");
+            if (double.TryParse(Console.ReadLine(), out double distance))
+            {
+                CalculateTravelTime(distance); // Рассчитываем и выводим время в пути
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ошибка: введите корректное число для расстояния.");
+                Console.ResetColor();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Основной цикл программы
+            while (true)
+            {
+                WaterTransport transport = new WaterTransport(); // Создаем новый объект водного средства
+                transport.Execute(); // Выполняем все операции
+
+                // Запрос на завершение программы
+                Console.Write("Введите 'стоп' для завершения программы или любую клавишу для продолжения: ");
+                string command = Console.ReadLine();
+                if (command.ToLower() == "стоп")
+                {
+                    break; // Выход из цикла и завершение программы
+                }
+            }
+        }
+    }
+}
---
+++ Практическая работа №17/Практическая работа №17/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Практическая работа №17")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("sborka")]
+[assembly: AssemblyProduct("Практическая работа №17")]
+[assembly: AssemblyCopyright("Copyright © sborka 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("e46e67c4-2312-44ef-8180-4f2f5bc1bd75")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
