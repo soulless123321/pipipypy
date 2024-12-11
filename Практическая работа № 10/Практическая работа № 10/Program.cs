9a354b86fc23efc371e14a0e406d4c5c547f9a7a
soulless123321 <ilasemonaev32232@gmail.com>
2024-12-11 03:24:40 +0300

    Add files via upload

---
+++ Практическая работа № 10/Практическая работа № 10/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая_работа___10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Введите размер квадратной матрицы (или 'стоп' для завершения): ");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "стоп")
+                {
+                    break; // Завершение программы
+                }
+
+                if (!int.TryParse(input, out int n) || n <= 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Ошибка: введите корректное положительное целое число.");
+                    Console.ResetColor();
+                    continue; // Переход к следующей итерации
+                }
+
+                int[,] matrix = new int[n, n];
+
+                // Ввод матрицы
+                Console.WriteLine("Введите элементы матрицы:");
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        while (true)
+                        {
+                            Console.Write($"Элемент [{i}, {j}]: ");
+                            if (int.TryParse(Console.ReadLine(), out matrix[i, j]))
+                            {
+                                break; // Выход из цикла при корректном вводе
+                            }
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Ошибка: введите корректное целое число.");
+                            Console.ResetColor();
+                        }
+                    }
+                }
+
+                // Поиск минимального по модулю диагонального элемента
+                int minIndex = 0;
+                for (int i = 1; i < n; i++)
+                {
+                    if (Math.Abs(matrix[i, i]) < Math.Abs(matrix[minIndex, minIndex]))
+                    {
+                        minIndex = i;
+                    }
+                }
+
+                // Запоминаем значение минимального по модулю диагонального элемента
+                int minValue = matrix[minIndex, minIndex];
+
+                // Изменение матрицы по условиям задачи
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (i == j)
+                        {
+                            matrix[i, j] = minValue; // Присвоение минимального диагонального элемента
+                        }
+                        else if (i < j)
+                        {
+                            matrix[i, j] = 1; // Элементы выше главной диагонали
+                        }
+                        else
+                        {
+                            matrix[i, j] = 2; // Элементы ниже главной диагонали
+                        }
+                    }
+                }
+
+                // Вывод измененной матрицы
+                Console.WriteLine("Измененная матрица:");
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.Write(matrix[i, j] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ Практическая работа № 10/Практическая работа № 10/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Практическая работа № 10")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("sborka")]
+[assembly: AssemblyProduct("Практическая работа № 10")]
+[assembly: AssemblyCopyright("Copyright © sborka 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("cf417bc4-263b-4351-b051-c7dec7274cf2")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
